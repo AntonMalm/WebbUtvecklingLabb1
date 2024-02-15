111190bc56e6a7ea9caa13c84b6c6609f0b1c76f
AntonMalm <anton.malm@iths.se>
2024-02-15 11:00:27 +0100

    s

--- JavaScripts/ProductTab.js
+++ JavaScripts/ProductTab.js
@@ -6,13 +6,15 @@ const products = [
       "../images/KebabPizzaProduct.webp",
     title: "KebabPizza",
     description:
-      "Våran populäraste pizza med svenskt nötkött tillsammans med världens godaste kebabsås",
+      "Våran populäraste pizza",
+    ModalDescription: "En kebabpizza är en läcker kombination av traditionell italiensk pizza toppad med saftigt, kryddat kebabkött. Den kompletteras med fräsch isbergssallad och krämig vitlökssås för en smakfull kontrast. Med varje tugga får du en explosion av smaker som tar din matupplevelse till nästa nivå.",
     price: 130,
   },
   {
     imageSrc: "../images/VesuvioProduct.webp",
     title: "Vesuvio",
-    description: "För dig som inte vill sticka ut, trots detta alltid god",
+    description: "Vesuvion är alltid god",
+    ModalDescription: "En Vesuvio-pizza är en klassisk italiensk skapelse med en perfekt balans av tomatsås och smält ost på en krispig deg. Den är enkel men ändå tillfredsställande, och dess renhet i smak gör den till en tidlös favorit. Dess namn, inspirerat av vulkanen Vesuvius, speglar dess ursprung i Neapel, Italien.",
     price: 110,
   },
   {
@@ -20,10 +22,10 @@ const products = [
       "../images/KebabIPitaProduct.webp",
     title: "Kebab i pita",
     description: "Pita i bröd, kan det bli godare?",
+    ModalDescription: "Kebab i pitabröd är en populär maträtt där saftigt, kryddat kebabkött packas in i ett mjukt och fluffigt pitabröd. Den kompletteras ofta med färska grönsaker och en krämig sås som ger en härlig kontrast till det kryddiga köttet. Denna rätt är både smakrik och mättande, vilket gör den till ett utmärkt val för en snabb och tillfredsställande måltid.",
     price: 99,
   },
 ];
-
 function displayProducts() {
   const productList = document.getElementById("productList");
 
@@ -36,8 +38,16 @@ function displayProducts() {
     const img = document.createElement("img");
     const price = document.createElement("p");
     const addButton = document.createElement("a");
+    const moreInfoButton = document.createElement("button");
+    const buttonDiv = document.createElement("div"); // Create a div to wrap the buttons
 
-    applyProductStyles(li, card, cardBody, cardTitle, cardDescription, addButton, price);
+    // Create unique id for each product modal
+    const modalId = "modal" + product.title.replace(/\s/g, '');
+
+    // Create modal for each product
+    const modal = createModal(modalId, product.ModalDescription);
+
+    applyProductStyles(li, card, cardBody, cardTitle, cardDescription, addButton, moreInfoButton, price);
 
     cardTitle.innerText = product.title;
     cardDescription.innerText = product.description;
@@ -48,30 +58,77 @@ function displayProducts() {
 
     price.innerText = product.price + " kr";
     addButton.innerText = "Lägg till";
+    moreInfoButton.innerText = "More Info";
 
     addButton.onclick = () => addToCart(product);
+    moreInfoButton.setAttribute("data-toggle", "modal");
+    moreInfoButton.setAttribute("data-target", "#" + modalId);
+
+    buttonDiv.classList.add("mt-auto"); // Add 'mt-auto' class to the button div
+    buttonDiv.appendChild(addButton);
+    buttonDiv.appendChild(moreInfoButton);
 
     cardBody.appendChild(img);
     cardBody.appendChild(cardTitle);
     cardBody.appendChild(cardDescription);
-    cardBody.appendChild(addButton);
+    cardBody.appendChild(buttonDiv); // Append the button div to the card body
     card.appendChild(cardBody);
     cardBody.appendChild(price);
     li.appendChild(card);
+    li.appendChild(modal);  // Append the modal to the product list item
 
     productList.appendChild(li);
   }
 }
 
-function applyProductStyles(li, card, cardBody, cardTitle, cardDescription, addButton, price) {
-  li.classList.add("col-sm", "list-group-item", "d-flex", "justify-content-center");
-  card.classList.add("card", "border-0", "card-style", "text-center", "m-3", "bg-dark");
+
+function createModal(id, ModalDescription) {
+  // Create modal elements
+  const modalDiv = document.createElement("div");
+  const modalDialog = document.createElement("div");
+  const modalContent = document.createElement("div");
+  const modalBody = document.createElement("div");
+  const modalDescription = document.createElement("p");
+
+  // Set modal attributes
+  modalDiv.classList.add("modal", "fade");
+  modalDiv.id = id;
+  modalDiv.setAttribute("tabindex", "-1");
+  modalDiv.setAttribute("role", "dialog");
+  modalDiv.setAttribute("aria-labelledby", id + "Label");
+  modalDiv.setAttribute("aria-hidden", "true");
+
+  modalDialog.classList.add("modal-dialog");
+  modalDialog.setAttribute("role", "document");
+
+  modalContent.classList.add("modal-content");
+
+  modalBody.classList.add("modal-body");
+  modalDescription.innerText = ModalDescription;
+
+  // Append elements to modal
+  modalBody.appendChild(modalDescription);
+  modalContent.appendChild(modalBody);
+  modalDialog.appendChild(modalContent);
+  modalDiv.appendChild(modalDialog);
+
+  return modalDiv;
+}
+
+
+
+function applyProductStyles(li, card, cardBody, cardTitle, cardDescription, addButton, moreInfoButton, price) {
+  li.classList.add("col-sm", "list-group-item", "d-flex", "justify-content-center", "bg-dark", "m-3"); // Add 'm-3' class here
+  card.classList.add("card", "border-0", "card-style", "text-center", "bg-dark", "rounded");
   cardBody.classList.add("card-body", "d-flex", "flex-column", "flex-grow-1", "text-white");
   cardTitle.classList.add("card-title");
   cardDescription.classList.add("card-text");
   addButton.classList.add("btn", "btn-primary", "btn-success", "mt-auto");
+  moreInfoButton.classList.add("btn", "btn-primary", "mt-auto", "btn-no-margin"); // Add 'btn-no-margin' class here
+
   price.classList.add("text-grey");
 }
+
 
 
 window.onload = function () {
@@ -83,6 +140,7 @@ function addToCart(product) {
   const cartList = document.getElementById("cartList");
 
   const li = document.createElement("li");
+  li.dataset.price = product.price; // Store the price in a data attribute
   const cartItem = document.createElement("div");
   const itemName = document.createElement("span");
   const removeButton = document.createElement("button");
@@ -90,7 +148,6 @@ function addToCart(product) {
   cost += product.price;
   var cartTotal = document.getElementById("CartTotal");
   cartTotal.innerText = "Kundvagnens pris: " + cost + "kr";
-
 
   cartItem.classList.add("cart-item", "d-flex", "mb-2", "p-2", "mg-4");
   itemName.innerText = product.title;
@@ -107,8 +164,10 @@ function addToCart(product) {
 
 function removeCartItem(cartItem) {
   const cartList = document.getElementById("cartList");
+  const price = Number(cartItem.dataset.price); // Retrieve the price from the data attribute
+
   cartList.removeChild(cartItem);
+  cost -= price;
+  var cartTotal = document.getElementById("CartTotal");
+  cartTotal.innerText = "Kundvagnens pris: " + cost + "kr";
 }
-
-
-
